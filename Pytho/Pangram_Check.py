cb5471f68c11e71e91b853b0afd6b802233516ad
Ajay <ajayidc007@gmail.com>
2020-01-15 17:42:05 +0530

    Update Pangram_Check.py

--- Pytho/Pangram_Check.py
+++ Pytho/Pangram_Check.py
@@ -1,12 +1,9 @@
 #Date: 1/15/2
 import string
-
 def ispangram(str1, alphabet=string.ascii_lowercase):
     
-    
-    
 #one_liner    
-#    return len(alphabet) == len(''.join(set(str1.lower().replace(' ',''))))
+    return len(alphabet) == len(''.join(set(str1.lower().replace(' ',''))))
     
 #sorted_method_tested    
 #    print(''.join(sorted(set(str1.lower().replace(' ','')))))
@@ -14,17 +11,11 @@ def ispangram(str1, alphabet=string.ascii_lowercase):
 #another_onliner  
 #    return set(alphabet) <= set(str1.lower())
     
-        
-    
-    
+#convenient method to understand
 #     cmplist=[]
 #     for i in str1.lower():
 #         if i in alphabet:
 #             cmplist.append(i)
 #     return len(alphabet) == len(''.join(set(cmplist)))
-
-
-
-
 
 print(ispangram("The quick brown fox jumps over the lazy dog"))
